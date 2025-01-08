c938fc1511786734da733c30ba0c03e667691a58
rodjat <rodjat@mail.ru>
2025-01-08 14:11:03 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,23 @@
+from os.path import join, dirname
+
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    BOT_TOKEN: SecretStr
+
+    WEBAPP_URL: str = "https://rodjat.github.io/whdocbot"
+
+    WEBHOOK_URL: str = "http://185.41.160.207"
+    WEBHOOK_PATH: str = "/webhook"
+
+    APP_HOST: str = "localhost"
+    APP_PORT: int = 8000
+
+    model_config = SettingsConfigDict(
+        env_file=join(dirname(__file__), ".env"),
+        env_file_encoding="utf-8"
+    )
+
+config = Config()
---
+++ main.py
@@ -0,0 +1,47 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message, Update, WebAppInfo
+from aiogram.filters import CommandStart
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram.client.default import DefaultBotProperties
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+import uvicorn
+
+from config import config
+
+
+bot = Bot(
+        config.BOT_TOKEN.get_secret_value(),
+        default=DefaultBotProperties(parse_mode="HTML")
+    )
+dp = Dispatcher()
+app = FastAPI()
+
+markup = (
+    InlineKeyboardBuilder()
+    .button(text="Open Me", web_app=WebAppInfo(url=config.WEBHOOK_URL))
+).as_markup()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Hello!", reply_markup=markup)
+
+
+@app.post(config.WEBHOOK_PATH)
+async def webhook(req: Request) -> None:
+    update = Update.model_validate(await req.json(), context={"bot": bot})
+    await dp.feed_update(bot, update)
+
+
+# async def main():
+#     await bot.delete_webhook(drop_pending_updates=True)
+#     await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=config.APP_HOST, port=config.APP_PORT)
