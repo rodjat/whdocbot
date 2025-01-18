c86595acaa7d9c4712fa80e4f24d023af21e0bca
rodjat <rodjat@mail.ru>
2025-01-18 21:24:06 +0500

    update config

--- config.py
+++ config.py
@@ -10,12 +10,11 @@ class Config(BaseSettings):
 
     WEBAPP_URL: str = "https://rodjat.github.io/whdocbot"
 
-    WEBHOOK_URL: str = "http://185.41.160.207"
+    WEBHOOK_URL: str = "https://185.41.160.207"
     WEBHOOK_PATH: str = "/webhook"
 
     APP_HOST: str = "localhost"
     APP_PORT: int = 8000
-    DB_PATH: str = "database.db"
 
     model_config = SettingsConfigDict(
         env_file=join(dirname(__file__), ".env"),
